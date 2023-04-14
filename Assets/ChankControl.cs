b83ea47a109904c6270140c0cbd7367c8da65016
Daniil Vdovin <45402557+DaniilVdovin@users.noreply.github.com>
2023-04-14 15:00:44 +0300

    upd move

--- Assets/ChankControl.cs
+++ Assets/ChankControl.cs
@@ -12,4 +12,15 @@ public class ChankControl : MonoBehaviour
     public bool WeRot = false;
     public Ttype type;
     public Transform[] Lines;
+    public GameObject[] Stoping;
+
+    public void Start()
+    {
+        if(type == Ttype.Floor)
+        if (Random.Range(0,5)==2)
+        {
+            Instantiate(Stoping[Random.Range(0, Stoping.Length - 1)],
+                Lines[Random.Range(0, Lines.Length - 1)].transform.position, Quaternion.identity, transform);
+        }
+    }
 }
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -6,17 +6,19 @@ using static UnityEngine.GraphicsBuffer;
 public class PlayerControl : MonoBehaviour
 {
     public Camera Camera;
+    public GameObject Map;
     public Transform CameraTarget;
     public Vector3 CameraOffset;
     public float Speed;
+    public float JumpForce;
     public bool CanJump = true;
     public bool isJump = false;
     public bool isRun = false;
     public bool isLive = true;
+    public bool isGround = true;
 
     private Animator Animator;
     private Rigidbody Rigidbody;
-    public int Line = 1;
     private ChankControl ChankNow;
 
     void Start()
@@ -26,62 +28,48 @@ public class PlayerControl : MonoBehaviour
     }
     private void Update()
     {
+        if (Physics.Raycast(new Ray(transform.position + Vector3.up * 2, Vector3.down), out RaycastHit hitC, 3f))
+        ChankNow = hitC.collider.GetComponent<ChankControl>();
         KeyManager();
-        if (isRun)
+        if (Physics.Raycast(new Ray(transform.position + Vector3.up * 2, Vector3.down), out RaycastHit hit, 10f))
+        {
+            if(isLive)
+                isRun = true;
+            isGround = true;
+            isJump = false;
+        } else isGround = false;
+        if (isRun && isGround)
         {
             Rigidbody.velocity = transform.forward * Speed;
         }
     }
     private void KeyManager()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space)) Jump();
+        switch (ChankNow.type)
         {
-            Jump();
-        }
-        Ray Ray = new Ray(transform.position + Vector3.up * 2, Vector3.down);
-        if (Physics.Raycast(Ray, out RaycastHit hit, 10f))
-        {
-            ChankNow = hit.collider.GetComponent<ChankControl>();
-            switch (ChankNow.type) {
-                case ChankControl.Ttype.Pivot:
-                    if (!ChankNow.WeRot)
-                    {
-                        if (Input.GetKeyDown(KeyCode.LeftArrow))
-                        {
-                            ChankNow.WeRot = true;
-                            transform.Rotate(Vector3.up, -90);
-                        }
-                        if (Input.GetKeyDown(KeyCode.RightArrow))
-                        {
-                            ChankNow.WeRot = true;
-                            transform.Rotate(Vector3.up, 90);
-                        }
-                    }
-                    break;
-                case ChankControl.Ttype.Floor:
+            case ChankControl.Ttype.Pivot:
+                CanJump = false;
+                if (!ChankNow.WeRot)
+                {
                     if (Input.GetKeyDown(KeyCode.LeftArrow))
                     {
-                        ChangeLine(ChankNow, true);
+                        ChankNow.WeRot = true;
+                        transform.Rotate(Vector3.up,-90);
                     }
                     if (Input.GetKeyDown(KeyCode.RightArrow))
                     {
-                        ChangeLine(ChankNow, false);
+                        ChankNow.WeRot = true;
+                        transform.Rotate(Vector3.up, 90);
                     }
-                    break;
-                default: break;
-            }
-
+                }
+                break;
+            case ChankControl.Ttype.Floor:
+                CanJump = true;
+                break;
+            default: break;
         }
 
-    }
-    private void ChangeLine(ChankControl chank, bool left)
-    {
-        if (left) Line--;
-        else Line++;
-        if (Line < 0) { Line = 0; return; };
-        if (Line > 2) { Line = 2; return; };
-        if (left)transform.position -= transform.right * 5;
-        else transform.position += transform.right * 5;
     }
     private void FixedUpdate()
     {
@@ -95,6 +83,12 @@ public class PlayerControl : MonoBehaviour
         Camera.transform.rotation = Quaternion.LookRotation(CameraTarget.transform.position - Camera.transform.position);
     }
     private void Jump() {
+        if (!CanJump) return; 
+        if (isJump) return;
+        isRun = false;
+        isJump = true;
+        isGround = false;
         Animator.SetTrigger("Jump");
+        Rigidbody.AddForce(100 * JumpForce * Vector3.up, ForceMode.Impulse);
     }
 }
