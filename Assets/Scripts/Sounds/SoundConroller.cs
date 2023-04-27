ba17b5aab8ecab722c0e27dc819917d0cea4af65
Artem-Shk <o.barbarisov@yandex.ru>
2023-04-27 12:51:05 +0300

    AudioSource

---
+++ Assets/Scripts/Sounds/SoundConroller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Sounds
+{
+    internal class SoundConroller: MonoBehaviour
+    {
+        public AudioSource Controller;
+        public AudioClip shieldSound;
+        public AudioClip PickSound;
+        public AudioClip DeathSound;
+        public AudioClip embiend;
+
+    }
+}
